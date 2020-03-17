09909a1b23cd57357912d73941d0430dc7cb6b26
Alexandr Plakhotnikov <mxplaha@gmail.com>
2020-03-17 13:58:30 +0300

    in test_main_page.py 2 methods how to navigate between pages

--- test_main_page.py
+++ test_main_page.py
@@ -2,14 +2,13 @@ from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
 
-def test_guest_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
-    page = MainPage(browser, link)
-    page.open()
-    # при 1 способе перехода между страницами
-    # login_page = page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+# при первом способе перехода межу страницами
+# def test_guest_can_go_to_login_page(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/'
+#     page = MainPage(browser, link)
+#     page.open()
+#     login_page = page.go_to_login_page()
+#     login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -17,3 +16,13 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+# при втором способе прехода между страницами
+def test_guest_can_go_to_login_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/'
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
