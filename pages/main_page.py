5c2e89707d038b3e183659cbef99eda0ab784820
Alexandr Plakhotnikov <mxplaha@gmail.com>
2020-03-17 13:22:18 +0300

    in main_page.py method go_to_login_page() in comment how to navigate between pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,7 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
+# для реализации перехода между страницами
+from .login_page import LoginPage
 
 
 class MainPage(BasePage):
@@ -7,6 +9,9 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        # переход между страницами
+        # инициализируем объект страницы с логином и возвращаем его
+        # 1 способ return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), 'Login link is not presented'
