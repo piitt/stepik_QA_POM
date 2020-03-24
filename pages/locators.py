b2b870bd9fa14fc6c55445f3cebd1fc712faa239
Alexandr Plakhotnikov <mxplaha@gmail.com>
2020-03-24 16:29:16 +0300

    in locators.py create class ProductPageLocators with 3 constants

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,9 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.ID, 'login_form')
     REGISTER_FORM = (By.ID, 'register_form')
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET = (By.CLASS_NAME, 'btn-add-to-basket')
+    PRODUCT_TITLE = (By.CSS_SELECTOR, 'div.product_main > h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, 'div.product_main > p.price_color')
